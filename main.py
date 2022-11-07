df31e30b2f09f5454a20f435cc8dc0a085afff61
Kuj1 <the.same.dentist@gmail.com>
2022-11-07 10:08:02 +0300

    refactored code

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import os
 from datetime import datetime
 
 import aiohttp
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
 API_KEY = '9csvVU6SRe4D4vlzkglWIiNQwbcrva4w'
 
@@ -15,6 +15,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 unchecked_proxies_file = os.path.join(os.getcwd(), 'data', 'proxies.txt')
 checked_proxies_dir = os.path.join(os.getcwd(), 'data')
+
 
 def modify_proxy(path_to_proxy):
     modified_proxy = set()
@@ -27,25 +28,25 @@ def modify_proxy(path_to_proxy):
     return modified_proxy
 
 
-async def check_proxy_api(api_key, proxy_list):
-    modified_proxys = modify_proxy(proxy_list)
-    for proxy in modified_proxys:
-        usefull_proxy = proxy.replace('http://', '').split('@')[::-1][0].split(':')[0]
+# async def check_proxy_api(api_key, proxy_list):
+#     modified_proxys = modify_proxy(proxy_list)
+#     for proxy in modified_proxys:
+#         usefull_proxy = proxy.replace('http://', '').split('@')[::-1][0].split(':')[0]
+#
+#         # resp = requests.get(f'https://ipqualityscore.com/api/json/ip/{api_key}/{usefull_proxy}')
+#         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+#             try:
+#                 # print(f'https://ipqualityscore.com/api/json/ip/{api_key}/{proxy}')
+#                 async with session.get(f'https://ipqualityscore.com/api/json/ip/{api_key}/{usefull_proxy}') as resp:
+#                     if resp.status == 200:
+#                         body = await resp.json()
+#                         print(body['fraud_score'])
+#                     else:
+#                         print(resp.status)
+#             except Exception as ex:
+#                 print(ex)
+#                 continue
 
-
-        # resp = requests.get(f'https://ipqualityscore.com/api/json/ip/{api_key}/{usefull_proxy}')
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-            try:
-                # print(f'https://ipqualityscore.com/api/json/ip/{api_key}/{proxy}')
-                async with session.get(f'https://ipqualityscore.com/api/json/ip/{api_key}/{usefull_proxy}') as resp:
-                    if resp.status == 200:
-                        body = await resp.json()
-                        print(body['fraud_score'])
-                    else:
-                        print(resp.status)
-            except Exception as ex:
-                print(ex)
-        
 
 async def check_proxies(url):
     proxies = modify_proxy(unchecked_proxies_file)
@@ -57,28 +58,27 @@ async def check_proxies(url):
             try:
                 async with session.get(url, proxy=proxy) as resp:
                     if resp.status == 200:
-                        print('OK')
-                        body = await resp.text()
-                        soup = BeautifulSoup(body, 'lxml')
+                        # print('OK')
+                        # body = await resp.text()
+                        # soup = BeautifulSoup(body, 'lxml')
                         # print(soup)
 
-                        ip = soup.find('div', class_ = 'ip').text.strip()
-                        loc = soup.find_all('div', class_='value-country').text.strip()
+                        # ip = soup.find('div', class_ = 'ip').text.strip()
+                        # loc = soup.find_all('div', class_='value-country').text.strip()
 
-                        print(f'{ip}\n{loc}\n')
+                        # print(f'{ip}\n{loc}\n')
 
                         with open(os.path.join(checked_proxies_dir, f'checked_proxy_{date_time}.txt'), 'a') as checked:
                             pre_proxy = proxy.replace('http://', '').split('@')[::-1]
                             original_proxy = ':'.join(pre_proxy)
                             checked.write(f'{original_proxy}\n')
                     else:
-                        print(f'{resp.status}')
-
-
+                        # print(f'{resp.status}')
+                        continue
 
             except Exception as ex:
-                    with open(os.path.join(checked_proxies_dir, f'log_{date_time}.txt'), 'a') as log:
-                        log.write(f'\n[PROXY]: {proxy}\n[ERROR]: {ex}\n' + '-' * 192)
+                with open(os.path.join(checked_proxies_dir, f'log_{date_time}.txt'), 'a') as log:
+                    log.write(f'\n[PROXY]: {proxy}\n[ERROR]: {ex}\n' + '-' * 192)
 
 if __name__ == '__main__':
     # asyncio.run(check_proxy_api(api_key=API_KEY, proxy_list=unchecked_proxies_file))
